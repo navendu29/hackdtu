a9ae034912f9e324efba8a10b7c4e57556fdef32
navendu29 <navendugupta29@gmail.com>
2017-02-10 23:23:48 +0530

    copy

--- health/urls.py
+++ health/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.conf.urls import include,url
+from django.contrib import admin
+
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^medicine/',include('medicine.urls')),
 ]
