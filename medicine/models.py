9cf13de5eb7ca030c4834d0a13940c3cb4e9b5a5
shubham <shubhamsj.3330@gmail.com>
2017-02-11 00:52:45 +0530

    added models

--- medicine/models.py
+++ medicine/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 
 from django.core.urlresolvers import reverse
+
+class Doctor(models.Model):
+    name=models.CharField(max_length=100)
+    degree=models.CharField(max_length=20)
+    designation=models.CharField(max_length=30)
+    doctor_dp=models.CharField(max_length=500)
+    contactno=models.IntegerField(max_length=10)
+    specialisation=models.CharField(max_length=40)
+    workplace=models.CharField(max_length=100)
+class Disease(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    disease=models.CharField(max_length=500)
+    prescription=models.CharField(max_length=100000)
